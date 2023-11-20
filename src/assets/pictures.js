2f7ca1d918e2b8d351845162646e43f46db4e351
xavierrice <xavier.rice@gmail.com>
2023-11-20 12:22:41 -0500

    probs with user2 img

--- src/assets/pictures.js
+++ src/assets/pictures.js
@@ -1,7 +1,7 @@
 import Professor from './ProfessorX.jpeg';
-import User2Img from './User2.jpeg';
+import User2img from './User2img'
 import Homepage from './Homepage.jpg';
 
-const imgObj = { "Professor":Professor, "User2":User2Img, "Homepage":Homepage};
+const imgObj = { "Professor":Professor, "User2":User2, "Homepage":Homepage};
 
 export default imgObj;
