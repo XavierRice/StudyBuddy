614c2def1c7912ec88f718049241bc67642dda9d
xavierrice <xavier.rice@gmail.com>
2023-11-20 12:28:52 -0500

    switch pictues tying to upload to netlify

--- src/assets/pictures.js
+++ src/assets/pictures.js
@@ -1,7 +1,8 @@
 import Professor from './ProfessorX.jpeg';
-import User2img from './User2img'
+import User2 from './user3.jpeg'
 import Homepage from './Homepage.jpg';
 
-const imgObj = { "Professor":Professor, "User2":User2img, "Homepage":Homepage};
+
+const imgObj = { "Professor":Professor, "User2":User2, "Homepage":Homepage};
 
 export default imgObj;
