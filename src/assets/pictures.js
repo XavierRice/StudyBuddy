13155253a310dd6d47e148025a3a3b2a6b53abad
xavierrice <xavier.rice@gmail.com>
2023-11-20 12:14:56 -0500

    workin on user2pics

--- src/assets/pictures.js
+++ src/assets/pictures.js
@@ -1,7 +1,7 @@
 import Professor from './ProfessorX.jpeg';
-import User2 from './User2.jpeg';
+import User2Img from './User2.jpeg';
 import Homepage from './Homepage.jpg';
 
-const imgObj = { "Professor":Professor, "User2":User2, "Homepage":Homepage};
+const imgObj = { "Professor":Professor, "User2":User2Img, "Homepage":Homepage};
 
 export default imgObj;
